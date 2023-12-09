823b6b6236561c66ea04dad77279a69a9a4e68b3
Rizky017 <148648238+Rizky017@users.noreply.github.com>
2023-12-09 20:42:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(session({
-  secret: 'secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: 'secret_key',
+//   resave: false,
+//   saveUninitialized: true
+// }));
 
 const prisma = new PrismaClient({
   datasources: {
@@ -30,28 +30,27 @@ const prisma = new PrismaClient({
   },
 });
 
-app.use((req, res, next) => {
-  res.locals.loggedIn = req.session.loggedIn || false;
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.loggedIn = req.session.loggedIn || false;
+//   next();
+// });
 
-app.get('/api/checkLoginStatus', (req, res) => {
-  res.json({ loggedIn: req.session.loggedIn || false });
-});
+// app.get('/api/checkLoginStatus', (req, res) => {
+//   res.json({ loggedIn: req.session.loggedIn || false });
+// });
 
 app.use('/signup', signupRouter);
 app.use('/login', loginRoutes);
 app.use('/appointment', appointmentRoutes);
 
 app.post('/logout', (req, res) => {
-  req.session.destroy(err => {
+  // req.session.destroy(err => {
     if (err) {
-      console.error('Error destroying session:', err);
+      console.error('Error :', err);
       return res.status(500).json({ error: 'Logout failed' });
     }
     res.status(200).json({ status: 'success', message: 'Logout successful' });
   });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
