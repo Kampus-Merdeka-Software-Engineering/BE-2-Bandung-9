d8aa9332df848cf3d7ad277e2605b6d8ec072977
Rizky017 <148648238+Rizky017@users.noreply.github.com>
2023-12-10 13:45:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-// const session = require('cookie-session');
-// const session = require('express-session');
 const { PrismaClient } = require('@prisma/client');
 const signupRouter = require('./routes/signup.routes');
 const loginRoutes = require('./routes/login.routes');
 const appointmentRoutes = require('./routes/appointment.routes');
 
-// const DATABASE_URL = 'postgresql://postgres:bD4-ba2BA4EF1CG1e25adBdE3D2gCg*E@roundhouse.proxy.rlwy.net:34839/railway';
 
 const app = express();
 const port = 3000;
@@ -16,35 +13,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(session({
-//   secret: 'secret_key',
-//   resave: false,
-//   saveUninitialized: true
-// }));
-
-// const prisma = new PrismaClient({
-//   datasources: {
-//     db: {
-//       url: DATABASE_URL,
-//     },
-//   },
-// });
-
-// app.use((req, res, next) => {
-//   res.locals.loggedIn = req.session.loggedIn || false;
-//   next();
-// });
-
-// app.get('/api/checkLoginStatus', (req, res) => {
-//   res.json({ loggedIn: req.session.loggedIn || false });
-// });
+app.get("/", async (req, res) => {
+  res.send("respon");
+});
 
 app.use('/signup', signupRouter);
 app.use('/login', loginRoutes);
 app.use('/appointment', appointmentRoutes);
 
 app.post('/logout', (req, res) => {
-  // req.session.destroy(err => {
     if (err) {
       console.error('Error :', err);
       return res.status(500).json({ error: 'Logout failed' });
