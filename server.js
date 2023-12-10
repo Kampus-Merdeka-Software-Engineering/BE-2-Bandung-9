a2f5bb145589ea755a94bc93c058fba7b1af3eb8
Rizky017 <148648238+Rizky017@users.noreply.github.com>
2023-12-10 13:37:44 +0700

    perubahan

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const session = require('express-session');
+// const session = require('cookie-session');
+// const session = require('express-session');
 const { PrismaClient } = require('@prisma/client');
 const signupRouter = require('./routes/signup.routes');
 const loginRoutes = require('./routes/login.routes');
 const appointmentRoutes = require('./routes/appointment.routes');
 
+const DATABASE_URL = 'postgresql://postgres:bD4-ba2BA4EF1CG1e25adBdE3D2gCg*E@roundhouse.proxy.rlwy.net:34839/railway';
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(session({
-  secret: 'secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: 'secret_key',
+//   resave: false,
+//   saveUninitialized: true
+// }));
 
 const prisma = new PrismaClient({
   datasources: {
@@ -27,29 +30,28 @@ const prisma = new PrismaClient({
   },
 });
 
-app.use((req, res, next) => {
-  res.locals.loggedIn = req.session.loggedIn || false;
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.loggedIn = req.session.loggedIn || false;
+//   next();
+// });
 
-app.get('/api/checkLoginStatus', (req, res) => {
-  res.json({ loggedIn: req.session.loggedIn || false });
-});
+// app.get('/api/checkLoginStatus', (req, res) => {
+//   res.json({ loggedIn: req.session.loggedIn || false });
+// });
 
 app.use('/signup', signupRouter);
 app.use('/login', loginRoutes);
 app.use('/appointment', appointmentRoutes);
 
 app.post('/logout', (req, res) => {
-  req.session.destroy(err => {
+  // req.session.destroy(err => {
     if (err) {
-      console.error('Error destroying session:', err);
+      console.error('Error :', err);
       return res.status(500).json({ error: 'Logout failed' });
     }
     res.status(200).json({ status: 'success', message: 'Logout successful' });
   });
-});
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`server is ready running at ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
